98de57899b50604cf5129042195d55e2e4ce0456
Glitchpix <davidrannaleet@gmail.com>
2024-09-02 16:07:26 +0200

    *Use Application instead of Platform

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -2,23 +2,13 @@
 #include <core/logger.hpp>
 
 // TODO: Test
-#include <platform/platform.hpp>
+#include <core/application.hpp>
 
 int main(void) {
-    MSG_FATAL("very %s", "bad");
-    MSG_ERROR("an error of %f", 3.4);
-    MSG_WARN("okay");
-    MSG_INFO("good");
-    MSG_DEBUG("detailed");
-    MSG_TRACE("even");
 
-    Platform* window{};
-    if (window->startup("Engine Testbed", 100, 100, 1280, 720)) {
-        while(true) {
-            window->pumpMessages();
-            window->sleep(100);
-        }
-    }
-    window->shutdown();
+    Application app{100, 100, 1280, 720, "Test Engine"};
+
+    app.run();
+    
     return 0;
 }
