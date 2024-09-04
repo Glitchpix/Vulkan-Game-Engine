44de9a7999588f5341307d50e167337c3faea281
Glitchpix <davidrannaleet@gmail.com>
2024-09-04 14:26:29 +0200

    +small memory mangement test

--- engine/src/entry.hpp
+++ engine/src/entry.hpp
@@ -2,12 +2,35 @@
 #include "core/application.hpp"
 #include "core/logger.hpp"
 #include "game_types.hpp"
+#include "core/e_memory.hpp"
 
 extern bool create_game(Game*);
 
 int main() {
-    Game game{};
 
+    Memory::initialize();
+    
+    // Test memory
+    //TODO: Remove this
+    const size_t blockSize = 1024*5;
+    void* testBlockA = Memory::allocate(blockSize, MEMORY_TAG_TEST);
+    Memory::zero(testBlockA, blockSize);
+    Memory::set(testBlockA, 1, blockSize);
+
+    void* testBlockB = Memory::allocate(blockSize, MEMORY_TAG_TEST);
+    Memory::zero(testBlockB, blockSize);
+    Memory::set(testBlockB, 1, blockSize);
+
+    MSG_INFO(Memory::get_usage());
+
+    MSG_INFO("Freeing blocks...")
+
+    Memory::free_block(testBlockA, blockSize, MEMORY_TAG_TEST);
+    Memory::free_block(testBlockB, blockSize, MEMORY_TAG_TEST);
+
+    MSG_INFO(Memory::get_usage());
+
+    Game game{};
     if(!create_game(&game)){
         MSG_FATAL("Game creation failed!");
         return -1;
@@ -24,6 +47,8 @@ int main() {
         MSG_FATAL("Application did not shutdown gracefully!")
         return -3;
     };
+
+    Memory::shutdown();
     
     return 0;
 }
