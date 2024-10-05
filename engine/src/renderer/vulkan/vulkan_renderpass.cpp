d25d6f3077574907b536873017c17fa8be476a33
Glitchpix <davidrannaleet@gmail.com>
2024-10-05 23:43:12 +0200

    +Added begin and end methods *Use swapchain to get image and depth format

--- engine/src/renderer/vulkan/vulkan_renderpass.cpp
+++ engine/src/renderer/vulkan/vulkan_renderpass.cpp
@@ -1,15 +1,19 @@
 #include "vulkan_renderpass.hpp"
+#include "core/logger.hpp"
 #include "vulkan/vulkan_core.h"
 #include "vulkan_defines.inl"
+#include "vulkan_swapchain.hpp"
 #include <array>
 
 
-RenderPass::RenderPass(VkDevice device, VkFormat imageColorFormat, VkFormat imageDepthFormat) : mDevice{device} {
+RenderPass::RenderPass(VkDevice device, const VulkanSwapchain& swapchain, VkRect2D renderArea,
+                       VkClearColorValue clearColor, VkClearDepthStencilValue depthStencil)
+    : mDevice{device}, mRenderArea{renderArea}, mClearColor{clearColor}, mDepthStencil{depthStencil} {
     VkSubpassDescription subpass{};
     subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
 
     VkAttachmentDescription colorAttachment{};
-    colorAttachment.format = imageColorFormat;
+    colorAttachment.format = swapchain.get_image_color_format();
     colorAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
     colorAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
     colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
@@ -23,7 +27,7 @@ RenderPass::RenderPass(VkDevice device, VkFormat imageColorFormat, VkFormat imag
     colorAttachmentRef.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
 
     VkAttachmentDescription depthAttachment{};
-    depthAttachment.format = imageDepthFormat;
+    depthAttachment.format = swapchain.get_image_depth_format();
     depthAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
     depthAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
     depthAttachment.storeOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
@@ -61,6 +65,26 @@ RenderPass::RenderPass(VkDevice device, VkFormat imageColorFormat, VkFormat imag
     renderPassInfo.pDependencies = &dependency;
 
     VK_CHECK(vkCreateRenderPass(mDevice, &renderPassInfo, nullptr, &mRenderpass));
+    MSG_INFO("[Vulkan] Successfully created renderpass: {:p}", static_cast<void*>(this));
 }
+
+void RenderPass::begin(VkFramebuffer framebuffer, VkCommandBuffer commandBuffer) {
+
+    std::array<VkClearValue, 2> clearValues{};
+    clearValues[0].color = mClearColor;
+    clearValues[1].depthStencil = mDepthStencil;
+
+    VkRenderPassBeginInfo beginInfo{};
+    beginInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+    beginInfo.framebuffer = framebuffer;
+    beginInfo.renderPass = mRenderpass;
+    beginInfo.renderArea = mRenderArea;
+    beginInfo.clearValueCount = clearValues.size();
+    beginInfo.pClearValues = clearValues.data();
+
+    vkCmdBeginRenderPass(commandBuffer, &beginInfo, VK_SUBPASS_CONTENTS_INLINE);
+}
+
+void RenderPass::end(VkCommandBuffer commandBuffer) { vkCmdEndRenderPass(commandBuffer); }
 
 RenderPass::~RenderPass() { vkDestroyRenderPass(mDevice, mRenderpass, nullptr); }
--- engine/src/renderer/vulkan/vulkan_renderpass.hpp
+++ engine/src/renderer/vulkan/vulkan_renderpass.hpp
@@ -2,12 +2,20 @@
 #include "defines.hpp"
 #include "vulkan/vulkan_core.h"
 
+class VulkanSwapchain;
 class RenderPass {
 public:
-    RenderPass(VkDevice device, VkFormat imageColorFormat, VkFormat imageDepthFormat);
+    RenderPass(VkDevice device, const VulkanSwapchain& swapchain, VkRect2D renderArea, VkClearColorValue clearColor,
+               VkClearDepthStencilValue depthStencil);
     ~RenderPass();
+
+    void begin(VkFramebuffer framebuffer, VkCommandBuffer commandBuffer);
+    void end(VkCommandBuffer commandBuffer);
 
 private:
     VkDevice mDevice{nullptr};
     VkRenderPass mRenderpass{nullptr};
+    VkRect2D mRenderArea{};
+    VkClearColorValue mClearColor{};
+    VkClearDepthStencilValue mDepthStencil{};
 };
