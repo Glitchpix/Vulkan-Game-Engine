9c5502a715d5c5e2a0eeef55115f8aee9909dc20
Glitchpix <davidrannaleet@gmail.com>
2024-10-05 23:40:34 +0200

    +Save format as member with getter *Minor refactoring, reordering of members

--- engine/src/renderer/vulkan/vulkan_image.cpp
+++ engine/src/renderer/vulkan/vulkan_image.cpp
@@ -7,7 +7,7 @@
 
 VulkanImage::VulkanImage(VulkanDevice& device, uint32_t width, uint32_t height, VkFormat format, VkImageTiling tiling,
                          VkImageUsageFlags usageFlags, VkMemoryPropertyFlags memoryProperties)
-    : mDevice{&device}, mWidth{width}, mHeight{height} {
+    : mDevice{&device}, mWidth{width}, mHeight{height}, mFormat{format} {
     VkImageCreateInfo imageInfo{};
     imageInfo.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
     imageInfo.imageType = VK_IMAGE_TYPE_2D;
@@ -16,7 +16,7 @@ VulkanImage::VulkanImage(VulkanDevice& device, uint32_t width, uint32_t height, 
     imageInfo.extent.depth = 1;
     imageInfo.mipLevels = 1;
     imageInfo.arrayLayers = 1;
-    imageInfo.format = format;
+    imageInfo.format = mFormat;
     imageInfo.tiling = tiling;
     imageInfo.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
     imageInfo.usage = usageFlags;
@@ -37,8 +37,8 @@ VulkanImage::VulkanImage(VulkanDevice& device, uint32_t width, uint32_t height, 
     VK_CHECK(vkAllocateMemory(mDevice->get_logical_device(), &allocInfo, nullptr, &mMemory));
 
     VK_CHECK(vkBindImageMemory(mDevice->get_logical_device(), mHandle, mMemory, 0));
-
-    create_image_view(format, VK_IMAGE_ASPECT_DEPTH_BIT);
+    // TODO: This should be configurable!
+    create_image_view(VK_IMAGE_ASPECT_DEPTH_BIT);
 }
 
 VulkanImage::~VulkanImage() {
@@ -53,12 +53,12 @@ VulkanImage::~VulkanImage() {
     }
 }
 
-void VulkanImage::create_image_view(VkFormat format, VkImageAspectFlags aspectFlags) {
+void VulkanImage::create_image_view(VkImageAspectFlags aspectFlags) {
     VkImageViewCreateInfo viewInfo{};
     viewInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
     viewInfo.image = mHandle;
     viewInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
-    viewInfo.format = format;
+    viewInfo.format = mFormat;
     viewInfo.subresourceRange.aspectMask = aspectFlags;
     viewInfo.subresourceRange.baseMipLevel = 0;
     viewInfo.subresourceRange.levelCount = 1;
--- engine/src/renderer/vulkan/vulkan_image.hpp
+++ engine/src/renderer/vulkan/vulkan_image.hpp
@@ -11,17 +11,20 @@ public:
                 VkImageUsageFlags usageFlags, VkMemoryPropertyFlags memoryProperties);
     ~VulkanImage();
 
+    [[nodiscard]] const VkFormat& get_format() const { return mFormat; }
+
 private:
+    VulkanDevice* mDevice;
+    uint32_t mWidth{0};
+    uint32_t mHeight{0};
+    VkFormat mFormat{};
+
     VkImage mHandle{nullptr};
     VkDeviceMemory mMemory{nullptr};
     VkImageView mImageView{nullptr};
 
-    VulkanDevice* mDevice;
 
-    uint32_t mWidth{0};
-    uint32_t mHeight{0};
-
-    void create_image_view(VkFormat format, VkImageAspectFlags aspectFlags);
+    void create_image_view(VkImageAspectFlags aspectFlags);
 
     uint32_t find_memory_type_index(uint32_t typeFilter, VkMemoryPropertyFlags properties);
 };
