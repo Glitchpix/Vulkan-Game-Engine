3e62c20d5a30df4537a8d74883b45f602b42ff59
Glitchpix <davidrannaleet@gmail.com>
2024-11-29 11:43:44 +0100

    +Implement negation, addition and subtraction for vec types

--- engine/src/math/vec.cpp
+++ engine/src/math/vec.cpp
@@ -9,3 +9,60 @@ bool operator==(const Vec3& lhs, const Vec3& rhs) {
 bool operator==(const Vec4& lhs, const Vec4& rhs) {
     return lhs.x == rhs.x && lhs.y == rhs.y && lhs.z == rhs.z && lhs.w == rhs.w;
 }
+
+
+Vec2 operator-(const Vec2& vec) {
+    return Vec2{{-vec.x}, {-vec.y}};
+}
+Vec3 operator-(const Vec3& vec) {
+    return Vec3{{-vec.x}, {-vec.y}, {-vec.z}};
+}
+Vec4 operator-(const Vec4& vec) {
+    return Vec4{{-vec.x}, {-vec.y}, {-vec.z}, {-vec.w}};
+}
+
+Vec2 operator+(const Vec2& lhs, const Vec2& rhs) {
+    return Vec2{{lhs.x + rhs.x}, {lhs.y + rhs.y}};
+}
+Vec3 operator+(const Vec3& lhs, const Vec3& rhs) {
+    return Vec3{{lhs.x + rhs.x}, {lhs.y + rhs.y}, {lhs.z + rhs.z}};
+}
+Vec4 operator+(const Vec4& lhs, const Vec4& rhs) {
+    return Vec4{{lhs.x + rhs.x}, {lhs.y + rhs.y}, {lhs.z + rhs.z}, {lhs.w + rhs.w}};
+}
+
+Vec2 operator-(const Vec2& lhs, const Vec2& rhs) {
+    return Vec2{{lhs.x - rhs.x}, {lhs.y - rhs.y}};
+}
+Vec3 operator-(const Vec3& lhs, const Vec3& rhs) {
+    return Vec3{{lhs.x - rhs.x}, {lhs.y - rhs.y}, {lhs.z - rhs.z}};
+}
+Vec4 operator-(const Vec4& lhs, const Vec4& rhs) {
+    return Vec4{{lhs.x - rhs.x}, {lhs.y - rhs.y}, {lhs.z - rhs.z}, {lhs.w - rhs.w}};
+}
+
+Vec2 dot(const Vec2& lhs, const Vec2& rhs) {
+    (void)rhs;
+    return lhs;
+}
+Vec3 dot(const Vec3& lhs, const Vec3& rhs) {
+    (void)rhs;
+    return lhs;
+}
+Vec4 dot(const Vec4& lhs, const Vec4& rhs) {
+    (void)rhs;
+    return lhs;
+}
+
+Vec2 cross(const Vec2& lhs, const Vec2& rhs) {
+    (void)rhs;
+    return lhs;
+}
+Vec3 cross(const Vec3& lhs, const Vec3& rhs) {
+    (void)rhs;
+    return lhs;
+}
+Vec4 cross(const Vec4& lhs, const Vec4& rhs) {
+    (void)rhs;
+    return lhs;
+}
--- tests/src/math/vec_test.hpp
+++ tests/src/math/vec_test.hpp
@@ -15,6 +15,12 @@ TEST_SUITE("Vec2") {
 
         CHECK(firstVec == secondVec);
     }
+    TEST_CASE("Equality - Mixed-sign values") {
+        Vec2 firstVec{{2}, {-2}};
+        Vec2 secondVec{{2}, {-2}};
+
+        CHECK(firstVec == secondVec);
+    }
     TEST_CASE("Not Equal - Mixed-sign values") {
         Vec2 firstVec{};
         Vec2 secondVec{};
@@ -28,6 +34,30 @@ TEST_SUITE("Vec2") {
             CHECK(firstVec != secondVec);
         }
     }
+    TEST_CASE("Negation - Mixed-sign values") {
+        Vec2 firstVec{};
+        firstVec.x = 2;
+        firstVec.y = -2;
+        firstVec = -firstVec;
+        CHECK(firstVec.x == -2);
+        CHECK(firstVec.y == 2);
+    }
+    TEST_CASE("Addition - Mixed-sign values") {
+        Vec2 firstVec{{2}, {-2}};
+        Vec2 secondVec{{2}, {-2}};
+
+        Vec2 resultVec = firstVec + secondVec;
+        CHECK(resultVec.x == 4);
+        CHECK(resultVec.y == -4);
+    }
+    TEST_CASE("Subtraction - Mixed-sign values") {
+        Vec2 firstVec{{2}, {-2}};
+        Vec2 secondVec{{2}, {-2}};
+
+        Vec2 resultVec = firstVec - secondVec;
+        CHECK(resultVec.x == 0);
+        CHECK(resultVec.y == 0);
+    }
 }
 
 TEST_SUITE("Vec3") {
@@ -40,6 +70,12 @@ TEST_SUITE("Vec3") {
     TEST_CASE("Equality - Default initialized values") {
         Vec3 firstVec{};
         Vec3 secondVec{};
+
+        CHECK(firstVec == secondVec);
+    }
+    TEST_CASE("Equality - Mixed-sign values") {
+        Vec3 firstVec{{2}, {-2}, {2}};
+        Vec3 secondVec{{2}, {-2}, {2}};
 
         CHECK(firstVec == secondVec);
     }
@@ -59,6 +95,34 @@ TEST_SUITE("Vec3") {
             firstVec.z = -1;
             CHECK(firstVec != secondVec);
         }
+    }
+    TEST_CASE("Negation - Mixed-sign values") {
+        Vec3 firstVec{};
+        firstVec.x = 2;
+        firstVec.y = -2;
+        firstVec.z = -1;
+        firstVec = -firstVec;
+        CHECK(firstVec.x == -2);
+        CHECK(firstVec.y == 2);
+        CHECK(firstVec.z == 1);
+    }
+    TEST_CASE("Addition - Mixed-sign values") {
+        Vec3 firstVec{{2}, {-2}, {1}};
+        Vec3 secondVec{{2}, {-2}, {-1}};
+
+        Vec3 resultVec = firstVec + secondVec;
+        CHECK(resultVec.x == 4);
+        CHECK(resultVec.y == -4);
+        CHECK(resultVec.z == 0);
+    }
+    TEST_CASE("Subtraction - Mixed-sign values") {
+        Vec3 firstVec{{2}, {-2}, {1}};
+        Vec3 secondVec{{2}, {-2}, {-1}};
+
+        Vec3 resultVec = firstVec - secondVec;
+        CHECK(resultVec.x == 0);
+        CHECK(resultVec.y == 0);
+        CHECK(resultVec.z == 2);
     }
 }
 
@@ -101,5 +165,37 @@ TEST_SUITE("Vec4") {
             firstVec.w = -1;
             CHECK(firstVec != secondVec);
         }
+    }
+    TEST_CASE("Negation - Mixed-sign values") {
+        Vec4 firstVec{};
+        firstVec.x = 2;
+        firstVec.y = -2;
+        firstVec.z = -1;
+        firstVec.w = 4;
+        firstVec = -firstVec;
+        CHECK(firstVec.x == -2);
+        CHECK(firstVec.y == 2);
+        CHECK(firstVec.z == 1);
+        CHECK(firstVec.w == -4);
+    }
+    TEST_CASE("Addition - Mixed-sign values") {
+        Vec4 firstVec{{2}, {-2}, {1}, {0}};
+        Vec4 secondVec{{2}, {-2}, {-1}, {-2}};
+
+        Vec4 resultVec = firstVec + secondVec;
+        CHECK(resultVec.x == 4);
+        CHECK(resultVec.y == -4);
+        CHECK(resultVec.z == 0);
+        CHECK(resultVec.w == -2);
+    }
+    TEST_CASE("Subtraction - Mixed-sign values") {
+        Vec4 firstVec{{2}, {-2}, {1}, {0}};
+        Vec4 secondVec{{2}, {-2}, {-1}, {-2}};
+
+        Vec4 resultVec = firstVec - secondVec;
+        CHECK(resultVec.x == 0);
+        CHECK(resultVec.y == 0);
+        CHECK(resultVec.z == 2);
+        CHECK(resultVec.w == 2);
     }
 }
